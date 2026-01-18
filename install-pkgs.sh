1ea588cd03e9d6ed42e4bfba32d310b37d13fc9e
Kaibi <40522793+Lahanig@users.noreply.github.com>
2026-01-18 20:29:14 +0300

    Update install-pkgs.sh

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -7,7 +7,7 @@ echo
 echo 'Installing pkgs...'
 echo
 
-sudo pacman -S hyprpaper swaync mpd fastfetch ncmpcpp wofi waybar kitty hyprshot resources nemo nemo-fileroller brightnessctl nwg-look code
+sudo pacman -S hyprpaper swaync mpd fastfetch ncmpcpp wofi waybar kitty hyprshot resources nemo nemo-fileroller brightnessctl nwg-look code gedit
 
 xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search # Set Nemo as default file browser
 gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty # Change the default terminal emulator for Nemo
