71ccfd052f4cc0159e91bf765677475e809591c0
Lahanig <lol355467@gmail.com>
2026-02-04 06:34:04 +0300

    Update install-pkgs.sh

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -13,7 +13,7 @@ sudo systemctl enable sddm
 xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search # Set Nemo as default file browser
 gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty # Change the default terminal emulator for Nemo
 
-yay -S opentabletdriver min-browser-bin protonplus
+yay -S opentabletdriver protonplus
 
 echo 
 echo 'Installing fonts'
