e30c5f8025d9ff3690b4a2b11470dc0f4408bc90
Lahanig <lol355467@gmail.com>
2026-08-05 18:35:55 +0300

    Updated wallpaper

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -7,7 +7,7 @@ echo
 echo 'Installing pkgs...'
 echo
 
-sudo pacman -S network-manager-applet sddm uwsm hyprland xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal-kde hyprpaper swaync mpd pamixer quodlibet nvim fish fastfetch ncmpcpp wofi waybar kitty hyprshot resources nemo nemo-fileroller brightnessctl nwg-look code gedit imagemagick swaybg yazi loupe bluez bluez-utils bluez-deprecated-tools blueman
+sudo pacman -S network-manager-applet sddm uwsm hyprland xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal-kde hyprpaper swaync mpd pamixer quodlibet nvim fish fastfetch ncmpcpp wofi waybar kitty hyprshot resources nemo nemo-fileroller brightnessctl nwg-look code gedit imagemagick swaybg yazi loupe bluez bluez-utils bluez-deprecated-tools blueman polkit-gnome
 
 sudo systemctl enable bluetooth.service
 
